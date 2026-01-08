be982e53c596bc920024f01568645f4211de56dd
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2026-01-08 17:09:20 +0200

    fix import modules

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -269,4 +269,4 @@ class Game {
   }
 }
 
-module.exports = Game;
+export default Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const Game = require('../modules/Game.class');
+import Game from '../modules/Game.class.js';
+
 const game = new Game();
 
 const cells = document.querySelectorAll('.field-cell');
